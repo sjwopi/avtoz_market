735c4b12760b29bc2c743603b0881c930d718aba
Ivan Teteruk <iteterk@gmail.com>
2024-03-25 23:04:51 +0300

    feat about page

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const hambBtn = document.querySelector('.header__hambbtn');
 const hambContainer = document.querySelector('.header__mobile-container');
-
 hambBtn.addEventListener('click', () => {
   hambBtn.classList.toggle('open')
   hambContainer.classList.toggle('open')
   document.body.classList.toggle('open')
 })
 
+const aboutMore = document.querySelector('.aboutPage__morebtn');
+const aboutText = document.querySelector('.aboutPage__container_sub')
+aboutMore.addEventListener('click', () => {
+  aboutText.classList.toggle('open')
+  if(aboutText.classList.contains('open')) {
+    aboutMore.textContent = "Скрыть"
+  } else {
+    aboutMore.textContent = "Читать далее"
+  }
+})
